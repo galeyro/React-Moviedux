ecc7403bdae6c8fd903307246ce6e7af33e30fab
galeyro <galo.guevara.torres@udla.edu.ec>
2025-04-19 15:16:59 -0500

    Cargar datos de peliculas desde .json public

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,8 +5,9 @@ export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const m = ["a", "b", "c"];
-    setMovies(m);
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
   }, []);
 
   return <div>{movies.length}</div>;
