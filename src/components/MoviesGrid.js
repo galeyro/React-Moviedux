c73ebad363afd316dbf4f6e1f674d59cbba541b0
galeyro <galo.guevara.torres@udla.edu.ec>
2025-04-19 16:17:13 -0500

    Corrección menor

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -18,7 +18,7 @@ export default function MoviesGrid() {
           <div className="movie-card-info">
             <h3 className="movie-card-title">{movie.title}</h3>
             <p className="movie-card-genre">{movie.genre}</p>
-            <p className="movie-card-genre">{movie.rating}</p>
+            <p className="movie-card-rating">{movie.rating}</p>
           </div>
         </div>
       ))}
